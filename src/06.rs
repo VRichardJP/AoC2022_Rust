d5a70abca49ad7d7bc360a5ace933714e6cfbda7
Vincent Richard <vincent.francois.richard@gmail.com>
2023-01-24 17:22:03 +0900

    add 06

---
+++ src/06.rs
@@ -0,0 +1,28 @@
+use anyhow::{Context, Result};
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    // part 1
+    let bytes = std::fs::read("data/06.txt")?;
+    let mut i = 4;
+    for window in bytes.windows(4) {
+        if window.iter().all_unique() {
+            break;
+        }
+        i += 1;
+    }
+    println!("{i}");
+
+    // part 1
+    let bytes = std::fs::read("data/06.txt")?;
+    let mut i = 14;
+    for window in bytes.windows(14) {
+        if window.iter().all_unique() {
+            break;
+        }
+        i += 1;
+    }
+    println!("{i}");
+
+    Ok(())
+}
